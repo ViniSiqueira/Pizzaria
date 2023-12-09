f939644a3bdfaf627e650e1052e11d729efd8f76
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2023-12-09 11:49:35 -0300

    adição de categoria e listagem

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,8 @@ import {CreateUserController} from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
 
 const router = Router()
 
@@ -11,5 +13,9 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export {router};
