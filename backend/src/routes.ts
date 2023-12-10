fefd3f14e6b26b4b6dfe935265b7e56e63484457
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2023-12-10 17:50:55 -0300

    adição de novo pedido e remoção

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,6 +8,8 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 import {CreateProductController} from './controllers/product/CreateProductController'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
+import {CreateOrderController} from './controllers/order/CreateOrderController'
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
 import uploadConfig from './config/multer'
 
 
@@ -28,5 +30,9 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export {router};
