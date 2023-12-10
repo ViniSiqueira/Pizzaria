b2452d49575be328d0244a8a763c1d521836fd04
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2023-12-10 18:40:49 -0300

    adição dos detalhes do pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,6 +14,7 @@ import { AddItemController } from './controllers/order/AddItemController'
 import { RemoveItemController } from './controllers/order/RemoveItemController'
 import { SendOrderController } from './controllers/order/SendOrderController'
 import {ListOrderController} from './controllers/order/ListOrderController'
+import {DetailOrderController} from './controllers/order/DetailOrderController'
 import uploadConfig from './config/multer'
 
 
@@ -46,5 +47,7 @@ router.delete('/order/remove', isAuthenticated, new RemoveItemController().handl
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 router.get('/orders', isAuthenticated, new ListOrderController().handle)
+
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
 
 export {router};
