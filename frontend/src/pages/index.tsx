b2564c23fbd06cf71b17839e9a8af8732aef9e25
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2024-01-04 21:12:47 -0300

    Adição de contexto

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,30 @@
+import { useContext, FormEvent, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/home.module.scss'
 import logoImg from '../../public/logo.svg'
 import {Input} from '../components/ui/Input'
 import { Button } from '../components/ui/Button'
+import { AuthContext } from '../contexts/AuthContext'
 
 export default function Home() {
+  const {signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const[loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email, 
+      password
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
     <Head>
@@ -15,16 +34,20 @@ export default function Home() {
       <Image src={logoImg} alt="Logo"/>
 
       <div className={styles.login}>
-        <form>
+        <form onSubmit={handleLogin}>
           
           <Input
             placeholder='Digite seu email'
             type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <Input
             placeholder='Digite sua senha'
             type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <Button
