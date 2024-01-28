59aaa55214663f3a95b8ae6150f7e40b35101e24
ViniSiqueira <vinicius_siqueirabravo@hotmail.com>
2024-01-28 11:22:00 -0300

    adicao de header e tela de categoria

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,25 @@
+import { useContext } from 'react'
 import styles from './styles.module.scss'
 import Link from 'next/link'
+import { FiLogOut } from 'react-icons/fi'
+import { AuthContext } from '../../contexts/AuthContext'
 
 export function Header(){
+    const { signOut} = useContext(AuthContext)
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <Link href="/dashboard">
                     <img src="/logo.svg" width={190} height={60}/>
                 </Link>            
-                <nav>                    
+                <nav className={styles.menuNav}>                    
                     <a href="/category">Categoria</a>                                        
-                    <a href="/product">Cardápio</a>                    
+                    <a href="/product">Cardápio</a>          
+
+                    <button onClick={signOut}>
+                        <FiLogOut color='#FFF' size={24}/>
+                    </button>          
                 </nav>
             </div>            
         </header>
